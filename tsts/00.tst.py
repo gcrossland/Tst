fe9e4e1e3c7e70459e548d7e8f72b1964353fa95
gcrossland <none@none>
2016-06-01 22:03:50 +0100

    Scrubbed machine-specific bits from tests' output

--- tsts/00.tst.py
+++ tsts/00.tst.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import itertools
 import codecs
+import re
 import difflib
 import shutil
 
@@ -16,18 +17,28 @@ def readFile (pathName):
   else:
     return []
 
+_SEP = re.escape(os.sep)
+_PATH_PATTERN = 'File "(.*' + _SEP + ')[^' + _SEP + ']+", line '
+
+def stripSpecifics (d):
+  while True:
+    match = re.search(_PATH_PATTERN, d)
+    if not match:
+      return d
+    d = d[:match.start(1)] + d[match.end(1):]
+
 D = "app*"
 
 def T (data):
   env = dict(os.environ)
   env['PYTHONPATH'] = str(os.path.join(data, "libraries")) + os.pathsep + env.get('PYTHONPATH', "")
   env['PYTHONIOENCODING'] = 'utf-8'
-  p = subprocess.Popen(cwd = data, env = env, args = [sys.executable, u"../../runtsts.py", "cmp"], stdin = None, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
+  p = subprocess.Popen(cwd = data, env = env, args = [sys.executable, os.path.join(os.pardir, os.pardir, "runtsts.py"), "cmp"], stdin = None, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
   out, err = p.communicate()
 
   t("rc {}", p.returncode)
-  t("out\n--------\n{}\n--------", out)
-  t("err\n--------\n{}\n--------", err)
+  t("out\n--------\n{}\n--------", stripSpecifics(out))
+  t("err\n--------\n{}\n--------", stripSpecifics(err))
   t("diffs\n--------")
   goodDirPathName = os.path.join(data, "tsts.good")
   outDirPathName = os.path.join(data, "tsts.out")
